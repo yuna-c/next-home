7e4c5edd385ac07bcaf9ebec9dba5760230cfea9
yuna <yuna744@gmail.com>
2024-10-21 03:15:29 +0900

    📝 feat: Pretendard 전역 폰트 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Homepage() {
   return (
     <div>
-      <h1 className="text-7xl text-cyan-600">Home Page</h1>
+      <h1 className="text-7xl font-bold">Home Page</h1>
     </div>
   )
 }
