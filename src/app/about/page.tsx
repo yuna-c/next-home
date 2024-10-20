825e31a6fd1b6b6572ef437b0ad93bb8e66cad2d
yuna <yuna744@gmail.com>
2024-10-21 03:53:38 +0900

    📝 feat: 중첩 경로 만들기

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function page() {
+export default function AboutPage() {
   return (
     <div>
       <h1 className="text-7xl font-bold">About Page</h1>
---
+++ src/app/info/contact/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function ContactPage() {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold">Contact Page</h1>
+      <Link href="/">Back home</Link>
+    </div>
+  )
+}
